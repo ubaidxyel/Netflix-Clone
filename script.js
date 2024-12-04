c7b6cb70aec401cc840b6ec3406aece496021f2f
ubaidxyel <midnightmarshal525@gmail.com>
2024-12-04 12:51:54 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// QUESTION 1
+
+const targetDiv = document.getElementById('answer1');
+const btn = document.getElementById('question1');
+btn.onclick = function () {
+    if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+    } else {
+        targetDiv.style.display = "block";
+    }
+}
+
+// QUESTION 2
+
+const question2 = document.getElementById('answer2');
+const question2Btn = document.getElementById('question2');
+question2Btn.onclick = function () {
+    if (question2.style.display !== "none") {
+        question2.style.display = "none";
+    } else {
+        question2.style.display = "block";
+    }
+}
+
+// QUESTION 3
+
+const question3 = document.getElementById('answer3');
+const question3Btn = document.getElementById('question3');
+question3Btn.onclick = function () {
+    if (question3.style.display !== "none") {
+        question3.style.display = "none";
+    } else {
+        question3.style.display = "block";
+    }
+}
+
+// QUESTION 4
+
+const question4 = document.getElementById('answer4');
+const question4Btn = document.getElementById('question4');
+question4Btn.onclick = function () {
+    if (question4.style.display !== "none") {
+        question4.style.display = "none";
+    } else {
+        question4.style.display = "block";
+    }
+}
+
+// QUESTION 5
+
+const question5 = document.getElementById('answer5');
+const question5Btn = document.getElementById('question5');
+question5Btn.onclick = function () {
+    if (question5.style.display !== "none") {
+        question5.style.display = "none";
+    } else {
+        question5.style.display = "block";
+    }
+}
+
+// QUESTION 6
+
+const question6 = document.getElementById('answer6');
+const question6Btn = document.getElementById('question6');
+question6Btn.onclick = function () {
+    if (question6.style.display !== "none") {
+        question6.style.display = "none";
+    } else {
+        question6.style.display = "block";
+    }
+}
